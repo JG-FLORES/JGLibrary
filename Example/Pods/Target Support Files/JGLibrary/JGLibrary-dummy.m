0d8f2eceff8309a2533d21f78504c8704551e151
JG Flores <guerraflores2012@gmail.com>
2021-01-20 10:32:13 -0600

    initial commit

---
+++ Example/Pods/Target Support Files/JGLibrary/JGLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JGLibrary : NSObject
+@end
+@implementation PodsDummy_JGLibrary
+@end
---
+++ Example/Pods/Target Support Files/Pods-JGLibrary_Example/Pods-JGLibrary_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JGLibrary_Example : NSObject
+@end
+@implementation PodsDummy_Pods_JGLibrary_Example
+@end
